204d875ce505b42b7f0c0eb80b4dc2c12d9e1247
Alvaro <alvgaona@gmail.com>
2023-12-28 01:03:31 -0300

    feat: added content - skills missing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  output: 'server',
+  adapter: vercel(),
 });
