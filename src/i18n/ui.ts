8d439640a9e3bf8ecb2e2ca5fa4d03d44747ae3c
Bel <mbgausachs@gmail.com>
2024-01-04 21:22:36 -0300

    squash

---
+++ src/i18n/ui.ts
@@ -0,0 +1,38 @@
+export const languages = {
+  en: 'English',
+  fr: 'Français',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'experience.title': 'Experience',
+    'education.title': 'Education',
+    'education.marketing.degree':
+      'Bachelor of Business Administration in Marketing',
+    'education.marketing.start-date': 'Mar. 2022',
+    'education.marketing.end-date': 'Present',
+    'education.strategic-mkt.degree':
+      'Master of Business Administration in Strategic Marketing',
+    'education.strategic-mkt.start-date': 'Apr. 2020',
+    'education.strategic-mkt.end-date': 'Dic. 2020',
+    'education.nutrition.degree': 'Bachelor of Science in Nutrition',
+    'education.nutrition.start-date': 'Mar. 2012',
+    'education.nutrition.end-date': 'Dic. 2017',
+  },
+  es: {
+    'experience.title': 'Experiencia',
+    'education.title': 'Educación',
+    'education.marketing.degree': 'Licenciatura en Marketing',
+    'education.marketing.start-date': 'Mar. 2022',
+    'education.marketing.end-date': 'Present',
+    'education.strategic-mkt.degree':
+      'Especialización en Dirección Estratégica de Marketing',
+    'education.strategic-mkt.start-date': 'Abr. 2020',
+    'education.strategic-mkt.end-date': 'Dec. 2020',
+    'education.nutrition.degree': 'Licenciatura en Nutrición',
+    'education.nutrition.start-date': 'Mar. 2012',
+    'education.nutrition.end-date': 'Dec. 2017',
+  },
+} as const;
